85e2b8823ce040dcb424cf4e8bdfe095461911b0
balu.s <balu.s@6dtech.co.in>
2019-09-17 15:51:03 +0530

    Request Format Changed

--- src/main/java/com/bank/app/BankAppApplication.java
+++ src/main/java/com/bank/app/BankAppApplication.java
@@ -9,5 +9,5 @@ public class BankAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankAppApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/bank/app/service/controllers/BankServices.java
+++ src/main/java/com/bank/app/service/controllers/BankServices.java
@@ -1,5 +1,7 @@
 package com.bank.app.service.controllers;
 
+import javax.validation.constraints.NotBlank;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,21 +10,17 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.app.data.dto.BankView;
 import com.bank.app.data.dto.Branches;
 import com.bank.app.data.repo.BankViewRepo;
 import com.bank.app.data.repo.BranchRepo;
-import com.bank.app.service.model.Request;
 import com.bank.app.service.model.Response;
 import com.bank.app.service.util.PaginationUtil;
-import com.bank.app.utils.ValidationGroups.GetBankDetails;
-import com.bank.app.utils.ValidationGroups.GetBankFromIfsc;
 
 @RestController
 @RequestMapping("/api")
@@ -36,13 +34,13 @@ public class BankServices {
 	@Autowired
 	BankViewRepo banksRepo;
 
-	@PostMapping("/bank/ifsc")
+	@GetMapping("/bank")
 	public ResponseEntity<Response> getBankDetailsFromIfsc(
-			@Validated(GetBankFromIfsc.class) @RequestBody Request request, Pageable pageable) {
+			@RequestParam("ifsc") @NotBlank String ifsc, Pageable pageable) {
 
-		logger.info("IFSC CODE : " + request.getIfscCode());
+		logger.info("IFSC CODE : " + ifsc);
 
-		Page<Branches> bankDetails = branchRepo.findByIfsc(request.getIfscCode(), pageable);
+		Page<Branches> bankDetails = branchRepo.findByIfsc(ifsc, pageable);
 		HttpHeaders headers = null;
 		Response response = new Response();
 		if (bankDetails.hasContent() && bankDetails.getContent().size() > 0) {
@@ -55,7 +53,7 @@ public class BankServices {
 			response.setDistrict(bankDetails.getContent().get(0).getDistrict());
 			response.setState(bankDetails.getContent().get(0).getState());
 			response.setStatus("Success");
-			headers = PaginationUtil.generatePaginationHttpHeaders(bankDetails, "/bank/ifsc");
+			headers = PaginationUtil.generatePaginationHttpHeaders(bankDetails, "/api/bank");
 		} else {
 			response.setStatus("Error");
 			response.setErrorMessage("Bank Details Not Found");
@@ -64,13 +62,13 @@ public class BankServices {
 		return new ResponseEntity<>(response, headers, HttpStatus.OK);
 	}
 
-	@PostMapping("/banks")
-	public ResponseEntity<Response> getBankDetails(@Validated(GetBankDetails.class) @RequestBody Request request,
-			Pageable pageable) {
+	@GetMapping("/banks")
+	public ResponseEntity<Response> getBankDetails(@RequestParam("bankName") @NotBlank String bankName, 
+			@RequestParam("city") @NotBlank String city, Pageable pageable) {
 
-		logger.info("Location: " + request.getBankName() + "," + request.getCity());
+		logger.info("Request: " + bankName + "," + city);
 
-		Page<BankView> bankDetails = banksRepo.findAllByBankNameAndCity(request.getBankName(), request.getCity(), pageable);
+		Page<BankView> bankDetails = banksRepo.findAllByBankNameAndCity(bankName, city, pageable);
 
 		Response response = new Response();
 		HttpHeaders headers = null;
